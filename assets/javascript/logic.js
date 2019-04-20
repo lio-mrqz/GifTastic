dd19842651657d0159b1cccfd3e5ffe057a596b9
Lio <lionell.marquez@gmail.com>
2019-04-20 03:55:49 -0400

    animation start/stop is working for the most part

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,4 @@
-var gifArr = ["Rainbow", "cat", "LOL"]
+var topics = ["Rainbow", "cat", "LOL"]
 
 function displayGif() {
     var search = $(this).attr("data-search");
@@ -20,13 +20,28 @@ function displayGif() {
           var p = $("<p>").text("Rating: " + rating);
 
           var searchImage = $("<img>");
-          searchImage.attr("src", results[i].images.fixed_height.url);
+          searchImage.addClass("gif").attr("src", results[i].images.fixed_width_still.url).attr("data-state", "still");
+
+          searchImage.attr("data-still", results[i].images.fixed_width_still.url)
+
+          searchImage.attr("data-animate",results[i].images.fixed_width.url);
 
           gifDiv.prepend(p);
           gifDiv.prepend(searchImage);
 
           $("#gifsResults").prepend(gifDiv);
-        }
+        } $(".gif").on("click", function() {
+            var state = $(searchImage).attr("data-state")
+            console.log(state)
+        
+            if (state === "still") {
+              $(searchImage).attr("src", $(searchImage).attr("data-animate"));
+              $(searchImage).attr("data-state", "animate")
+            } else {
+              $(searchImage).attr("src", $(searchImage).attr("data-still"));
+              $(searchImage).attr("data-state", "still")
+            }
+        });
       });
 };
 
@@ -34,9 +49,9 @@ function renderButtons() {
 
     $("#buttons-view").empty();
 
-    for (var i = 0; i < gifArr.length; i++) {
+    for (var i = 0; i < topics.length; i++) {
       var btn = $("<button>");
-      btn.addClass("gifBtn").attr("data-search", gifArr[i]).text(gifArr[i]);
+      btn.addClass("gifBtn").attr("data-search", topics[i]).text(topics[i]);
       $("#buttons-view").append(btn);
     }
 }
@@ -44,7 +59,7 @@ function renderButtons() {
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
     var gif = $("#gifInput").val().trim();
-    gifArr.push(gif);
+    topics.push(gif);
 
     renderButtons();
 });
